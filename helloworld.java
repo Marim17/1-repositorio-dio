8514093424ab53922b8b6f13574c5b05f89e2855
Marim17 <mmv_17@hotmail.com>
2022-07-16 20:03:54 -0300

    alteração no nome da classe

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-class QuickStart{
-    public static void main(String[] args){
-        System.out.println("Hello world!");   
+class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
