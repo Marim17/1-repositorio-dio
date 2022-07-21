17ef3a8e24de2b8f7a4ed7f5bb93d817b8ee9296
Marim17 <mmv_17@hotmail.com>
2022-07-21 17:06:02 -0300

    mais um

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,8 @@
-class helloworld {
+package 1-repositorio-dio;
+
+public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    
     }
 }
